920f18ea4075a64f757c6c9d12a70e4c20570387
Koli45330808 <akashkoli6417@gmail.com>
2025-10-08 16:01:52 +0530

    Time: 383 ms (69.96%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,7 +1,4 @@
-SELECT
-    p.firstName,
-    p.lastName,
-    a.city,
-    a.state
-FROM Person p
-LEFT JOIN Address a ON p.personId = a.personId;
+SELECT lastName,firstName, city ,state  
+FROM Person
+LEFT JOIN Address
+ON Person. personId=Address. personId ;
