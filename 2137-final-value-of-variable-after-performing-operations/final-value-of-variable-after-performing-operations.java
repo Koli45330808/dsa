97a3395d7af05ff03d16991904104fa91bef8f60
Aakash Koli <akashkoli6417@gmail.com>
2025-10-20 09:01:18 +0530

    Time: 1 ms (74.53%) | Memory: 42.8 MB (46.14%) - LeetSync

---
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int sum=0;
+        for(int i=0;i<=operations.length-1;i++){
+        
+            if(operations[i].equals("++X") || operations[i].equals("X++")){
+                sum=sum+1;
+
+            }
+            else{
+                sum=sum-1;
+            }
+        }
+
+        return sum;
+        
+    }
+}
