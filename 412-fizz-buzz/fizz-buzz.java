f3257709e1fca4cd6a2d2bc6c1894cf82b5550a3
Aakash Koli <akashkoli6417@gmail.com>
2025-11-24 22:50:47 +0530

    Time: 1 ms (99.82%) | Memory: 46.6 MB (29.79%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> sc=new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(i%3==0 && i%5==0){
+                sc.add("FizzBuzz");
+            }
+            else{
+                if(i%3==0){
+                    sc.add("Fizz");
+
+                }
+                else{
+                    if(i%5==0){
+                        sc.add("Buzz");
+                    }
+                    else{
+                        String ask=Integer.toString(i);
+                        sc.add(ask);
+                    }
+                }
+            }
+        }
+        return sc;
+    }
+}
