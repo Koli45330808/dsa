e35b7bd9e512ba5d7a8b7d952db9577dbe70d25e
Aakash Koli <akashkoli6417@gmail.com>
2026-03-11 11:23:05 +0530

    Time: 3 ms (6.69%) | Memory: 42.7 MB (6.13%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.java
@@ -4,21 +4,20 @@ class Solution {
 
         String binary = Integer.toBinaryString(n);
         char ch[]=binary.toCharArray();
-        int temp[]=new int[ch.length];
+        //int temp[]=new int[ch.length];
+        String str="";
         for(int i=0;i<ch.length;i++){
             if(ch[i]=='0'){
-                temp[i]=1;
+                str=str+1;
 
             }
             if(ch[i]=='1'){
-                 temp[i]=0;
+                 str=str+0;
             }
         }
         
-        String str="";
-        for(int i=0;i<temp.length;i++){
-            str=str+temp[i];
-        }
+        
+        
 
         int num = Integer.parseInt(str, 2);
 
