c861bec0cb25980c8c8f284777c9ace780ba963e
Aakash Koli <akashkoli6417@gmail.com>
2026-05-22 21:52:48 +0530

    Time: 437 ms (29.74%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,4 +1,4 @@
-SELECT firstName,lastName, city  ,state 
+SELECT  lastName,firstName,city ,state  
 FROM Person
 LEFT JOIN Address
-ON  Person.personId=Address. personId;
+ON Person.personId =Address.personId;
