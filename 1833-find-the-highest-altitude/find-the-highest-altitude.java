3f1aa0f74576d68351faa8ba2a276e1235d13ef3
Aakash Koli <akashkoli6417@gmail.com>
2026-06-19 12:40:49 +0530

    Time: 1 ms (4.86%) | Memory: 43.3 MB (6.14%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.java
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.java
@@ -3,19 +3,39 @@ class Solution {
 
 
 
+        ArrayList<Integer> sc=new ArrayList<>();
+
+
         int ask=0;
-        int result=0;
+    
+        int n=gain.length;
+        
+        sc.add(ask);
+        int i=0;
 
-        for(int akash:gain){
-            ask=ask+akash;
-            result=Math.max(result,ask);
+        while(n>0){
+            ask=ask+gain[i];
+            sc.add(ask);
+            i++;
+            n--;
 
+        }
 
+        int temp[]=new int[sc.size()];
+
+        for(int j=0;j<sc.size();j++){
+
+              temp[j]=sc.get(j);
+        }
+
+          int result=0;
+        for(int k=0;k<temp.length;k++){
+            result=Math.max(result,temp[k]);
         }
 
 
         return result;
-        
+
         
     }
 }
